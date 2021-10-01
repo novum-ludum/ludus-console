5f77af68cf2396d950b5d8c0a436d2f8f73d6d79
elegantShock2258 <elegantshock3307@gmail.com>
2021-10-01 12:51:00 +0530

    Wrote ASCIILogos Tests

--- src/main/java/tests/ASCIILogosTest.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-import com.ludus.ASCIILogos;
-
-public class ASCIILogosTest {
-    public static void main(String[] args) {
-        System.out.println(ASCIILogos.getRand());
-    }
-}
